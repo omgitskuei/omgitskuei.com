3129d00ed4fb4ee0028f8f458389d70fa8234890
Kuei <57666754+omgitskuei@users.noreply.github.com>
2023-10-05 13:46:25 +0800

    feat: add favicon and initial content changes

--- omgitskueicom/src/app/page.tsx
+++ omgitskueicom/src/app/page.tsx
@@ -3,6 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <link
+        rel="icon"
+        href="/assets/favicon.PNG"
+        type="image/png"
+        sizes="285x285"
+      />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -17,11 +23,11 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/profile_pic.PNG"
+              alt="Kuei-Feng Tung Profile picture"
               className="dark:invert"
-              width={100}
-              height={24}
+              width={285}
+              height={285}
               priority
             />
           </a>
