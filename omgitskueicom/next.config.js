35ac9138ccc46c64af454fadccaa23168cc02a21
Kuei-Feng Tung <57666754+omgitskuei@users.noreply.github.com>
2023-12-29 15:31:37 +0800

    feat: add internationalization, currently not working

--- omgitskueicom/next.config.js
+++ omgitskueicom/next.config.js
@@ -28,11 +28,10 @@ const nextConfig = {
         },
     },
 
-    // i18n: {
-    //     locales: ['en', 'zh-CN'],
-    //     defaultLocale: 'en',
-    //     localeDetection: false,
-    // },
+    i18n: {
+        locales: ['en-US', 'zh-HANT', ],
+        defaultLocale: 'en-US',
+    },
     // async headers() {
     //     return [
     //         {
