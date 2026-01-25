d77badd10c19d613eba619f80d85937222ad440f
kueiRad <166793708+kueiRad@users.noreply.github.com>
2026-01-25 23:33:36 +0800

    WIP portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,53 +3,42 @@ import Link from "next/link";
 
 export default function Page() {
     return (
-        <section style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            width: "100%",
-        }}>
-            <h1>
-                Portfolio
-            </h1>
-            <div>
-                {/* Web Apps */}
-                <section style={{
-                    display: "flex",
-                    gap: "10px",
-                    border: "1px solid white",
-                    width: "1180px"
-                }}>
-                    <Link href={"/portfolio/app"}>
-                        <h2>Web Apps</h2>
+        <article style={{ width: "100%" }}>
+            {/* Header */}
+            <section style={{
+                display: "flex",
+                justifyContent: "space-between",
+                // width: "80%",
+                marginTop: "20px",
+                padding: "20px",
+            }}>
+                <h1>Portfolio</h1>
+                <div>yes</div>
+            </section>
+            {/* Categories */}
+            <section>
+                <div>
+                    <Link href={"/portfolio/app"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <Image src={"/imgs/ui/pc.svg"} alt={"Games"} width={128} height={128}></Image>
+                        <h2>Web Apps</h2>
+                        <p>Various projects </p>
                     </Link>
-                </section>
-                {/* Mini Games */}
-                <section style={{
-                    display: "flex",
-                    gap: "10px",
-                    border: "1px solid white",
-                    width: "1180px"
-                }}>
-                    <Link href={"/portfolio/game"}>
-                        <h2>Mini Games</h2>
+                </div>
+                <br />
+                <div>
+                    <Link href={"/portfolio/game"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <Image src={"/imgs/ui/console-controller.svg"} alt={"Games"} width={128} height={128}></Image>
+                        <h2>Mini Games</h2>
                     </Link>
-                </section>
-                {/* UI Components */}
-                <section style={{
-                    display: "flex",
-                    gap: "10px",
-                    border: "1px solid white",
-                    width: "1180px"
-                }}>
-                    <Link href={"/portfolio/components"}>
-                        <h2>UI Components</h2>
+                </div>
+                <br />
+                <div>
+                    <Link href={"/portfolio/components"} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <Image src={"/imgs/ui/id-card.svg"} alt={"Games"} width={128} height={128}></Image>
+                        <h2>UI Components</h2>
                     </Link>
-                </section>
-            </div>
-        </section>
+                </div>
+            </section>
+        </article>
     );
 }
