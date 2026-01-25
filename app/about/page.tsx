fd3748761125037239bf4b634e3174673c46c4c3
kueiRad <166793708+kueiRad@users.noreply.github.com>
2026-01-25 23:33:25 +0800

    WIP about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,24 @@
 import styles from "./page.module.css";
 
 export default function Page() {
-  return (
-    <>
-      About
-    </>
-  );
+    return (
+        <article style={{ overflowY: "scroll" }}>
+            {/* Meet the developer */}
+            <section>
+                <h1>Meet the developer</h1>
+
+            </section>
+            {/* Purpose */}
+            <section>
+                <h1>Purpose of this website</h1>
+
+            </section>
+            {/* How to use */}
+            <section>
+                <h1>How to use this website</h1>
+
+
+            </section>
+        </article>
+    );
 }
