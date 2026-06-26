99d32472965de088e7900c53635eb4634dfc1058
Kuei <57666754+omgitskuei@users.noreply.github.com>
2026-06-26 16:26:06 +0800

    Finish Breadcrumb component

--- app/personal/redacted/page.tsx
+++ app/personal/redacted/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 import RedactedTerminal from "@/components/Terminal/RedactedTerminal";
+import { Breadcrumbs } from "@/components/Breadcrumbs";
 
 export default function Page() {
     return (
         <>
+            <Breadcrumbs items={[]} separator="/"></Breadcrumbs>
             <RedactedTerminal></RedactedTerminal>
         </>
     );
