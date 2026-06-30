10ea40807116129cce84d1e8db9cdf7475d84f92
Kuei <57666754+omgitskuei@users.noreply.github.com>
2026-06-30 18:00:53 +0800

    Personal page add breadcrumb

--- app/personal/page.tsx
+++ app/personal/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import { Breadcrumbs } from "@/components/Breadcrumbs";
 
 export default function Page() {
     return (
         <>
-            <Link href={"/personal/blog"}>Blog</Link>
+            <Breadcrumbs></Breadcrumbs>
         </>
     );
 }
