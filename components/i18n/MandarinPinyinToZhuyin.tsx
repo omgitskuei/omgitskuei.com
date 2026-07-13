3861a61fc5241063c030aa3feaad75cc5a3e5d50
Kuei <57666754+omgitskuei@users.noreply.github.com>
2026-07-13 15:45:58 +0800

    Standardize i18n nomeclature to componentMap.ts

--- components/i18n/MandarinPinyinToZhuyin.tsx
+++ components/i18n/MandarinPinyinToZhuyin.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from "react";
-import { zhuyinMap } from "./mandarinZhuyinMap";
+import { zhuyinMap } from "./mandarinPinyinToZhuyinMap";
 
 export const MandarinPinyinToZhuyin = ({
     baseText,
