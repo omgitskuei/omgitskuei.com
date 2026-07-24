4c14594f8f9ea450e792d3074f5754466f625242
omgitskuei <57666754+omgitskuei@users.noreply.github.com>
2026-07-24 17:32:56 +0800

    webapps page wip

--- app/professional/projects/react/webapps/page.tsx
+++ app/professional/projects/react/webapps/page.tsx
@@ -29,27 +29,109 @@ export default function Page() {
                 ]}>
             </Breadcrumbs>
             <h1>Web Apps</h1>
-            <Link href={"/professional/projects/react/webapps/bubblewrap"}>
-                {"Bubble Wrap"}
-            </Link>
-            <Link href={"/professional/projects/react/webapps/calculator"}>
-                {"Calculator"}
-            </Link>
-            <Link href={"/professional/projects/react/webapps/chess"}>
-                {"Chess"}
-            </Link>
-            <Link href={"/professional/projects/react/webapps/diceroll"}>
-                {"Dice Roll"}
-            </Link>
-            <Link href={"/professional/projects/react/webapps/terminal"}>
-                {"Terminal"}
-            </Link>
-            <Link href={"/professional/projects/react/webapps/tictactoe"}>
-                {"Tic Tac Toe"}
-            </Link>
-            <Link href={"/professional/projects/react/webapps/todolist"}>
-                {"To-Do List"}
-            </Link>
+
+            {/* Filter */}
+            <section style={{
+                alignSelf: "stretch", marginLeft: "10%", marginRight: "10%",
+                marginTop: "10px",
+                background: "white"
+            }}>
+                <h2>Filter</h2>
+
+                <h3>Categories</h3>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Productivity</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Utilities</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Social</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Shopping</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Art</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Music</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Health & Fitness</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Entertainment</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Education</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="" id="" />
+                    <label htmlFor="">Finance</label>
+                </div>
+
+                <h3>Sort</h3>
+                <fieldset>
+                    <legend>Sort</legend>
+                    <div>
+                        <input type="radio" name="radioFilterSort" id="radioFilterSortYear" value={"Year"} />
+                        <label htmlFor="radioFilterSortYear">Year</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="radioFilterSort" id="radioFilterSortDifficulty" value={"Difficulty"} />
+                        <label htmlFor="radioFilterSortYear">Difficulty</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="radioFilterSort" id="radioFilterSortName" value={"Name"} />
+                        <label htmlFor="radioFilterSortYear">Name</label>
+                    </div>
+                </fieldset>
+                
+                
+
+            </section>
+
+
+            <section style={{
+                alignSelf: "stretch", marginLeft: "10%", marginRight: "10%",
+                marginTop: "10px",
+                background: "white",
+                display: "flex", flexDirection: "column"
+            }}>
+                <h2>Results</h2>
+
+                <Link href={"/professional/projects/react/webapps/bubblewrap"}>
+                    {"Bubble Wrap"}
+                </Link>
+                <Link href={"/professional/projects/react/webapps/calculator"}>
+                    {"Calculator"}
+                </Link>
+                <Link href={"/professional/projects/react/webapps/chess"}>
+                    {"Chess"}
+                </Link>
+                <Link href={"/professional/projects/react/webapps/diceroll"}>
+                    {"Dice Roll"}
+                </Link>
+                <Link href={"/professional/projects/react/webapps/terminal"}>
+                    {"Terminal"}
+                </Link>
+                <Link href={"/professional/projects/react/webapps/tictactoe"}>
+                    {"Tic Tac Toe"}
+                </Link>
+                <Link href={"/professional/projects/react/webapps/todolist"}>
+                    {"To-Do List"}
+                </Link>
+            </section>
         </>
     );
 }
